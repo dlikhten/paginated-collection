c580084b554886792abfba9e43d77b359ae377c1
Dmitriy Likhten <dlikhten@gmail.com>
2013-01-12 22:46:33 -0500

    Add and remove now work individually vs resetting the whole collection

--- backbone-paginated-collection.js
+++ backbone-paginated-collection.js
@@ -48,31 +48,140 @@ SOFTWARE.
       this.collection = data.collection;
       this.perPage = data.perPage || this.perPage;
       var collection = this.collection;
-      collection.on("add", this.resetCollection, this);
-      collection.on("remove", this.resetCollection, this);
-      collection.on("reset", this.resetCollection, this);
-      collection.on("sort", this.resetCollection, this);
+      collection.on("add", this._add, this);
+      collection.on("remove", this._remove, this);
+      collection.on("reset", this.reset, this);
+      collection.on("sort", this.reset, this);
       this.changePage();
+    }
+
+    ,_add: function(model, collection, options) {
+      var where = (options && options.at != null) ? options.at : this.collection.models.length - 1;
+      var paginatedOffset = this.paginatedOffset(where);
+
+      if (paginatedOffset < this.perPage) {
+        // always remove the last element, that's always shifted over
+        var removeIndex = this.models.length - 1;
+        var toRemove = this.models[removeIndex];
+        this.remove(toRemove);
+        this.triggerRemove(toRemove, removeIndex, options);
+
+        var toAdd;
+        var toAddIndex;
+        if (paginatedOffset >= 0) {
+          toAdd = model;
+          toAddIndex = paginatedOffset;
+        }
+        else {
+          // we need an extra 1 index (no -1 in calculation) because the previous element shifted by 1
+          toAdd = this.collection.models[this.offset()];
+          toAddIndex = 0;
+        }
+        this.add(toAdd, {at: toAddIndex});
+        this.triggerAdd(toAdd, toAddIndex, options);
+      }
+    }
+
+    // when a model is removed from the underlying collection one of three things will be true
+    ,_remove: function(model, collection, options) {
+      var toRemove = this.modelToRemove(model, options.index);
+
+      // if toRemove is null, then the model removed comes after
+      // the current page, thus this page is not affected
+      if (toRemove) {
+        var removalIndex = this.indexOf(toRemove);
+        Backbone.Collection.prototype.remove.call(this, toRemove);
+
+        // total pages changed because it calculates based on
+        // the underlying model. So we compare if the current page
+        // is above the total pages (1 offset
+        if (this.page === this.lastPage() + 1) {
+          this.changePage(this.lastPage());
+        }
+        else if (this.isLastPage()) {
+          this.triggerRemove(toRemove, removalIndex, options);
+        }
+        else {
+          this.triggerRemove(toRemove, removalIndex, options);
+
+          // we're somewhere not on the last page, therefore we must trigger an
+          // add event for the one model that got shifted over, which is actually the
+          // last model in this page, so just sync and trigger that
+          // add event if appropriate
+          this.syncPage();
+
+          var addIndex = this.models.length - 1;
+          this.triggerAdd(this.models[addIndex], addIndex, options);
+        }
+      }
+    }
+
+    ,triggerRemove: function(toRemove, index, origOptions) {
+      if (! origOptions || ! origOptions.silent) {
+        this.trigger("remove", toRemove, this, {index: index});
+      }
+    }
+    ,triggerAdd: function(toAdd, index, origOptions) {
+      if (! origOptions || ! origOptions.silent) {
+        this.trigger("add", toAdd, this, {index: index})
+      }
+    }
+
+    ,offset: function(extraPages) {
+      extraPages = extraPages || 0;
+      return ((this.page + extraPages) * this.perPage);
+    }
+
+    ,modelToRemove: function(desiredModel, index) {
+      if (this.isRemovalOnCurrentPage(desiredModel)) {
+        return desiredModel;
+      }
+      else if (index <= this.offset()) {
+        return this.models[0];
+      }
+      else {
+        return null;
+      }
+    }
+
+    ,paginatedOffset: function(index) {
+      return index - this.offset();
+    }
+
+    ,isRemovalOnCurrentPage: function(model) {
+      return this.indexOf(model) !== -1;
+    }
+
+    ,reset: function() {
+      this.changePage();
+      this.trigger("reset", this);
     }
 
     ,totalPages: function() {
       return Math.ceil(this.collection.length / this.perPage);
     }
 
-    ,resetCollection: function() {
-      this.changePage();
-      this.trigger("reset", this);
+    ,lastPage: function() {
+      return this.totalPages() - 1;
+    }
+
+    ,isLastPage: function() {
+      return this.page === this.lastPage();
+    }
+
+    ,syncPage: function() {
+      var offset = this.offset();
+      var end = offset + this.perPage;
+
+      var slice = this.collection.models.slice(offset, end);
+      Backbone.Collection.prototype.reset.call(this, slice, {silent: true});
     }
 
     ,changePage: function(pageNumber) {
-      var page = (this.page = pageNumber || 0);
-      var perPage = this.perPage;
-      var offset = page * perPage;
-      var end = offset + perPage;
+      this.page = pageNumber || 0;
 
-      var slice = this.collection.models.slice(offset, end);
-      this.reset(slice, {silent: true});
-      this.trigger("paginated", page, this);
+      this.syncPage();
+      this.trigger("paginated", this.page, this);
     }
 
     ,_onModelEvent: function() {
--- spec/javascripts/backbone-paginated-collection-spec.js
+++ spec/javascripts/backbone-paginated-collection-spec.js
@@ -14,6 +14,12 @@ describe("Backbone.PaginatedCollection", function() {
   var allModels;
   var collection;
 
+  var modelRemoved;
+  var removedIndex;
+  var modelAdded;
+  var addedIndex;
+  var paginatedCount;
+
   beforeEach(function() {
     allModels = new RegularModelCollection();
     for(var i = 0; i < 10; i++) {
@@ -21,6 +27,27 @@ describe("Backbone.PaginatedCollection", function() {
     }
 
     collection = new ModelCollection(null, {collection: allModels, perPage: 2});
+  });
+
+  beforeEach(function() {
+    modelRemoved = null;
+    removedIndex = null;
+    modelAdded = null;
+    addedIndex = null;
+    paginatedCount = 0;
+
+    collection.bind("remove", function(model, collection, options) {
+      modelRemoved = model;
+      removedIndex = options.index;
+    });
+    collection.bind("add", function(model, collection, options) {
+      modelAdded = model;
+      addedIndex = options.index;
+    });
+    collection.bind("paginated", function() {
+      paginatedCount += 1;
+
+    });
   });
 
   describe("#totalPages", function() {
@@ -72,6 +99,177 @@ describe("Backbone.PaginatedCollection", function() {
       collection.changePage(1);
       collection.changePage(2);
       expect(triggered).toEqual(0);
+    });
+  });
+
+  describe("event:add", function() {
+    it("shifts the collection right if element added before current page", function() {
+      collection.changePage(1)
+      toBeAdded = allModels.models[1];
+      toBeRemoved = allModels.models[3];
+      allModels.add(new TehModel({value: 11}), {at: 0})
+
+      expect(addedIndex).toEqual(0);
+      expect(removedIndex).toEqual(1);
+      expect(modelAdded).toEqual(toBeAdded);
+      expect(modelRemoved).toEqual(toBeRemoved);
+    });
+
+    it("does nothing if element added after current page", function() {
+      collection.changePage(1);
+      allModels.add(new TehModel({value: 11}), {at: 9});
+
+      expect(modelAdded).toEqual(null);
+      expect(addedIndex).toEqual(null);
+      expect(modelRemoved).toEqual(null);
+      expect(removedIndex).toEqual(null);
+    });
+
+    it("inserts the new element, shifts everything right if added on current page", function() {
+      collection.changePage(1)
+      toBeAdded = new TehModel({value: 11});
+      toBeRemoved = allModels.models[3];
+      allModels.add(toBeAdded, {at: 2})
+
+      expect(addedIndex).toEqual(0);
+      expect(removedIndex).toEqual(1);
+      expect(modelAdded).toEqual(toBeAdded);
+      expect(modelRemoved).toEqual(toBeRemoved);
+    });
+
+    it("inserts the new element, removes last element if added at end of current page", function() {
+      collection.changePage(1)
+      toBeAdded = new TehModel({value: 11});
+      toBeRemoved = allModels.models[3];
+      allModels.add(toBeAdded, {at: 3})
+
+      expect(addedIndex).toEqual(1);
+      expect(removedIndex).toEqual(1);
+      expect(modelAdded).toEqual(toBeAdded);
+      expect(modelRemoved).toEqual(toBeRemoved);
+    });
+  });
+
+  describe("event:remove", function() {
+    it("does nothing if element added after current page", function() {
+      collection.changePage(0);
+      allModels.remove(allModels.models[9]); // last model
+
+      expect(modelRemoved).toBeFalsy();
+      expect(modelAdded).toBeFalsy();
+      expect(removedIndex).toEqual(null);
+      expect(addedIndex).toEqual(null);
+    });
+
+    it("removes removed element, and adds one from next page", function() {
+      collection.changePage(0);
+      var toRemove = allModels.models[0];
+      var toAdd = allModels.models[2];
+
+      allModels.remove(toRemove); // last model
+
+      expect(modelRemoved).toEqual(toRemove);
+      expect(modelAdded).toEqual(toAdd);
+      expect(removedIndex).toEqual(0);
+      expect(addedIndex).toEqual(1);
+    });
+
+    it("shifts the collection left if element removed before current page", function() {
+      collection.changePage(1);
+      var toRemove = allModels.models[2];
+      var toAdd = allModels.models[4];
+
+      allModels.remove(allModels.models[0]); // last model
+
+      expect(modelRemoved).toEqual(toRemove);
+      expect(modelAdded).toEqual(toAdd);
+      expect(removedIndex).toEqual(0);
+      expect(addedIndex).toEqual(1);
+    });
+
+    it("paginates one back back if last element on last page removed", function() {
+      allModels.add(new TehModel({value: 11}))
+      collection.changePage(5);
+      expect(collection.length).toEqual(1);
+
+      paginatedCount = 0;
+      allModels.remove(allModels.models[10]); // last model
+
+      expect(modelRemoved).toBeFalsy();
+      expect(removedIndex).toEqual(null);
+      expect(modelAdded).toBeFalsy();
+      expect(addedIndex).toEqual(null);
+
+      expect(paginatedCount).toEqual(1);
+    });
+
+    it("removes element on last page, does not paginate if one model on last page removed", function() {
+      collection.changePage(4);
+      expect(collection.length).toEqual(2);
+
+      toRemove = allModels.models[8];
+      allModels.remove(toRemove); // last model
+
+      expect(modelRemoved).toEqual(toRemove);
+      expect(removedIndex).toEqual(0);
+      expect(modelAdded).toBeFalsy();
+      expect(addedIndex).toEqual(null);
+    });
+  });
+
+  describe("#lastPage", function() {
+    it("should return the page index of the last page", function() {
+      expect(collection.lastPage()).toEqual(collection.totalPages() - 1);
+    });
+  });
+
+  describe("#isLastPage", function() {
+    it("should return true if on the last page", function() {
+      expect(collection.isLastPage()).toBeFalsy();
+
+      collection.page = collection.totalPages() - 1;
+
+      expect(collection.isLastPage()).toBeTruthy();
+    });
+  });
+
+  describe("#modelToRemove", function() {
+    it("should return the given model if that model is on the current page", function() {
+      expect(collection.modelToRemove(collection.models[0], 0)).toEqual(collection.models[0])
+    });
+
+    it("should return null if the collection is on page 0 and the model not in the current view", function() {
+      expect(collection.modelToRemove(allModels.models[9], 9)).toBeNull();
+    });
+
+    it("should return the first element of the collection if the model is on a previous page", function() {
+      collection.changePage(2);
+      expect(collection.modelToRemove(allModels.models[0], 0)).toEqual(collection.models[0]);
+    });
+
+    it("should return null if the element is on the following page", function() {
+      collection.changePage(2);
+      expect(collection.modelToRemove(allModels.models[9], 9)).toBeNull();
+    });
+  });
+
+  describe("#paginatedOffset", function() {
+    it("returns a number > this.perPage if the index is in a proceeding page", function() {
+      collection.changePage(2);
+
+      expect(collection.paginatedOffset(6)).toEqual(2);
+    });
+
+    it("returns a negative number if in a previous page", function() {
+      collection.changePage(2);
+
+      expect(collection.paginatedOffset(0)).toEqual(-4);
+    });
+
+    it("return a positive number < this.perPage if in current page", function() {
+      collection.changePage(2);
+
+      expect(collection.paginatedOffset(4)).toEqual(0);
     });
   });
 
